171f6dfeac8a7b672bcae99aca6aa24991298c85
Tomide <tomideso@gmail.com>
2021-07-04 16:33:55 +0100

    made userid unique

--- src/entity/Account.ts
+++ src/entity/Account.ts
@@ -3,7 +3,7 @@ import * as mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const AccountSchema = new Schema({
-  userId: String,
+  userId: { type: String, unique: true },
   subscription: [{ type: Schema.Types.ObjectId, ref: "Subscription" }],
 }, {
   timestamps: true
